48c66709be72cd69bb6d0acb8fc7cc18bd595202
Cleyton <cleytontyukrs62@gmail.com>
2022-06-21 17:48:25 -0300

    Estrutura sequencial

--- Exercicio 3.py
+++ Exercicio 3.py
@@ -1,4 +1,5 @@
-"""Faça um Programa que peça dois números e imprima a soma."""
+"""Faça um Programa que peça dois números e imprima a soma.
+Exercicio 3"""
 if __name__ == '__main__':
     num1 = int(input('Digite um numero: '))
     num2 = int(input('Digite mais um numero: '))
