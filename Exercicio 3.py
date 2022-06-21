488df747d1597bee90c09ed65891d5205656b057
Cleyton <cleytontyukrs62@gmail.com>
2022-06-21 17:43:15 -0300

    Exercicio 3

---
+++ Exercicio 3.py
@@ -0,0 +1,6 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+if __name__ == '__main__':
+    num1 = int(input('Digite um numero: '))
+    num2 = int(input('Digite mais um numero: '))
+    soma = num1 + num2
+    print(f'A soma dos números informados é: {soma}')
