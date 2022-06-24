4367f63a77ae5add048f9a65d2ecc3981ef9e816
Cleyton <cleytontyukrs62@gmail.com>
2022-06-24 17:22:31 -0300

    Estrutura sequencial

---
+++ Exercicio 5.py
@@ -0,0 +1,7 @@
+"""Faça um Programa que converta metros para centímetros."""
+
+if __name__ == '__main__':
+
+ area = float(input('Digite uma medida em Metros: '))
+ conv = area * 100
+ print(f'sua area mede', conv, 'Centimetros')
