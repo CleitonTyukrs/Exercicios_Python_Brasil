121572243dac49ca3a20da14cdf921010f6f616e
Cleyton <cleytontyukrs62@gmail.com>
2022-06-24 17:42:58 -0300

    Estrutura sequencial

---
+++ Exercicio 6.py
@@ -0,0 +1,6 @@
+"""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
+
+if __name__ == '__main__':
+    raio = float(input('Digite o raio do circulo: '))
+    area = 3.14 * (raio ** 2)
+    print(f'o tamanho da area do circulo é =', area)
